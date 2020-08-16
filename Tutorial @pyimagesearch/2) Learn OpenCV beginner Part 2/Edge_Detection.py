0d1fc2c7e92db311bdb2810e9d9b53f1ed10d3e0
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-08-16 21:02:56 +0530

    Made some changes in code of Edge Detection

--- Tutorial @pyimagesearch/2) Learn OpenCV beginner Part 2/Edge_Detection.py
+++ Tutorial @pyimagesearch/2) Learn OpenCV beginner Part 2/Edge_Detection.py
@@ -7,19 +7,21 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
+ap.add_argument("-m", "--minT", required=True,
+	help="minimum threshold for edge detection")
+ap.add_argument("-M", "--maxT", required=True,
+	help="minimum threshold for edge detection")
 args = ap.parse_args()
 
 # load the input image (whose path was supplied via command line
 # argument) and display the image to our screen
 image = cv2.imread(args.image)
 cv2.imshow("Image", image)
-cv2.waitKey(0)
 # convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", gray)
-cv2.waitKey(0)
 
-edged = cv2.Canny(gray, 30, 150)#Higher the threshold, lesser the edges detected (based on how much
+edged = cv2.Canny(gray, int(args.minT), int(args.maxT))#Higher the threshold, lesser the edges detected (based on how much
 #difference there is in both sides of the edge
 cv2.imshow("Edged", edged)
 cv2.waitKey(0) 
